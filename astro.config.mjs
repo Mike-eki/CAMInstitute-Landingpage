67dfa5bd11a2ca2f879ab8b24cbae800c9d76caf
mike-eki <miqueas.moreno2016@gmail.com>
2025-08-30 20:47:23 -0300

    Add base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://mike-eki.github.io/",
-    base: "",
+    base: '/CAMInstitute-Landingpage',
 
     experimental: {
         fonts: [
